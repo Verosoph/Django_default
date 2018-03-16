578a239821a9bad8265c6349635d4ac0285e01f6
Marco Mettke <mettkemarco@gmail.com>
2018-03-16 13:59:38 +0100

    it works so far

---
+++ .env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mrm/Projekte/marmetProjekte/Django_default/.env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Django_default
 default python django dev-environment with user authentification
+
+
+
+### Database
+SQLite
+Superuser is created as user:admin password: password123
